1d49fd9b825bf1dc929797b550ee04bacba9b4ba
Umauro <felipe.vega.14@sansano.usm.cl>
2017-06-02 17:33:44 -0400

    update

--- Buscador de Herramientas de Aprendizaje/url_Obtainer.py
+++ Buscador de Herramientas de Aprendizaje/url_Obtainer.py
@@ -1,4 +1,3 @@
-
 import json
 import urllib.request, urllib.parse
 
@@ -7,6 +6,7 @@ class urlObtainer():
         self.listaConfiable = confiable
 
     def urlGetter(self, queryString):
+        urlList = []
         for dominio in self.listaConfiable:
             query = urllib.parse.urlencode({'q': queryString+" :"+dominio})
             url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyDDKM1YteR8id4T1GwOQlTcm_agVGAwQUQ&cx=006325309441217833254:vcjnhphmc4i&' + query
@@ -17,11 +17,21 @@ class urlObtainer():
                 data = results['items']
                 #print(data)
                 resultado = data[0]['link']
+                urlList.append(resultado)
                 print(resultado)
 
             except TypeError:
-                print("D:")
+                print("Error al buscar, por favor intente más tarde (?)")
 
+        return urlList
+
+'''
+Uso de la clase
+
+Para instanciarla
 urlObtain = urlObtainer(['hyperphysics','wikipedia'])
 
+
+Para usar su método
 urlObtain.urlGetter('Campo Electrico')
+'''
