a3697f6c560c7c27a8e95d326dd541df1a1d5229
Umauro <felipe.vega.14@sansano.usm.cl>
2017-06-03 00:36:48 -0400

    Clase query para interaccion con DB

---
+++ Buscador de Herramientas de Aprendizaje/query.py
@@ -0,0 +1,128 @@
+import sqlite3 as lite
+
+class query():
+    def __init__(self, *args, **kwargs):
+        self.databaseName = 'database.db'
+        self.con = None
+        self.cur = None
+
+    def isActived(self):
+        if(self.con != None):
+            return True
+        else:
+            return False
+
+    def isClosed(self):
+        if(self.con == None):
+            return True
+        else:
+            return False
+
+    def conectar(self):
+        self.con = lite.connect(self.databaseName)
+        self.cur = self.con.cursor()
+
+    def desconectar(self):
+        self.cur = None
+        self.con.close()
+
+    def insert(self, *args, **kwargs):
+        try:
+            querySql = "INSERT INTO " + kwargs['tabla'] +"(Dominio, URL) VALUES(\'"+ kwargs['dominio'] +"\',\'"+ kwargs['url'] +"\');"
+            print(querySql)
+            self.cur.execute(querySql)
+            self.con.commit()
+        except AttributeError:
+            print("Error en la cantidad de atributos")
+        except TypeError:
+            print("Error en tipos de atributos")
+        except OperationalError:
+            print("Error en la consulta sql")
+        except lite.DatabaseError:
+            print("Error con la base de datos"+str(lite.DatabaseError[0]))
+
+
+
+    def select(self, campos = "*", *args, **kwargs):
+        try:
+            querySql = "SELECT "+ campos +" FROM " +kwargs['tabla'] + ";"
+            self.cur.execute(querySql)
+            rows = self.cur.fetchall()
+
+        except AttributeError:
+            print("Error en la cantidad de atributos")
+        except TypeError:
+            print("Error en tipos de atributos")
+        except OperationalError:
+            print("Error en la consulta sql")
+        except lite.DatabaseError:
+            print("Error con la base de datos: ")
+        return rows
+
+
+    def update(self, *args, **kwargs):
+        try:
+            querySql = "UPDATE "+ kwargs['tabla'] + " SET Dominio=\'"+ kwargs['dominioNuevo']+"\', URL=\'"+ kwargs['url']+"\' WHERE Dominio =\'"+kwargs['dominioAntiguo']+"\';"
+            print(querySql)
+            self.cur.execute(querySql)
+            self.con.commit()
+
+        except AttributeError:
+            print("Error en la cantidad de atributos")
+        except TypeError:
+            print("Error en tipos de atributos")
+        except OperationalError:
+            print("Error en la consulta sql")
+        except lite.DatabaseError:
+            print("Error con la base de datos")
+
+"""
+Función: insert(*args, **kwargs)
+
+Función para insertar elementos a la base de datos utilizada
+
+Uso:
+    llamar a la función de la siguiente manera
+    .query(tabla = <nombre_tabla>, dominio = <nombre_dominio>, url = <URL>)
+
+    Donde <nombre_tabla> = {Confiable, ListaNegra}
+"""
+
+"""
+Función: select(campos = "*", *args, **kwargs)
+
+Función para seleccionar elementos desde la base de datos
+
+Uso:
+    llamar a la función de la siguiente manera
+    .select(<campos Requeridos>, tabla = '<nombre_tabla>')
+
+
+    Donde <nombre_tabla> = {Confiable, ListaNegra}
+    En atributo campo debe ser un string con los campos requeridos desde la tabla
+    EJ:
+        .select("Dominio, URL", tabla="Confiable")
+
+Retorno:
+    Una lista de tuplas con los campos requeridos
+"""
+
+"""
+Función update(*args, **kwargs)
+
+Función para realizar Update de campos en la base de datos
+Uso:
+    llamar a la función de la siguiente manera
+    .update(tabla = <nombre_tabla>, dominioNuevo = <nuevo_dominio>, url = <nueva_url>, dominioAntiguo = <dominio_antiguo>)
+    Donde <nombre_tabla> = {Confiable, ListaNegra}
+"""
+
+"""
+Ejemplos de uso
+queryClass = query()
+queryClass.conectar()
+queryClass.insert(tabla="Confiable", dominio="Testo", url="httpASHDAHD")
+queryClass.select('Dominio',tabla='Confiable')
+queryClass.select(tabla='Confiable')
+queryClass.update(tabla='Confiable', dominioNuevo='Me Updatearon', url='meUpdatearon.com', dominioAntiguo='Testo')
+"""
